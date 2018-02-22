259957b9dfc794b8fd33a82b2a64bd717e6eefb3
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2018-02-22 19:20:22 +0200

    Added minimum viable cli

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
 mod compiler;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
 
-    compiler::compile(String::from("var foo : string := \"test\";"));
+    if args.len() < 2 {
+        println!("Usage: ./plint filename");
+        return;
+    }
+
+    let filename = &args[1];
+
+    println!("=== 'compile' file {} ===", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut source = String::new();
+    f.read_to_string(&mut source)
+        .expect("unable to read file");
+
+    compiler::compile(source);
 
     // compiler::run();
 }
